876cd6eb7a247494fa0160f43868ac3895f9a906
Pascal Mouret <pascal.mouret@avrios.com>
2017-11-15 23:10:33 +0100

    enemy throwing

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,13 @@
-extends KinematicBody2D
+extends "thrower.gd"
 
 onready var player_class = preload("res://scripts/player.gd")
+onready var ball = preload("res://ball.tscn")
 
 export var acceleration = 50
 export var friction = 50
 export var max_velocity = 50
 export var min_distance = 300
+export var throw_interval = 2
 
 var player = null
 var velocity = Vector2(0, 0)
@@ -31,10 +33,21 @@ func move_towards_player(delta):
 		velocity = (velocity + (vector * -1 * friction)).clamped(0)
 
 	move(velocity)
+	
+func start_throw_timer():
+	var timer = Timer.new()
+	timer.connect("timeout", self, "_on_throw_timer_timeout")
+	timer.set_wait_time(throw_interval)
+	timer.start()
+	add_child(timer)
 
-func _on_detector_body_enter( body ):
-	print(body)
-	if body extends player_class:
-		print("yaaay")
+func _on_detector_body_enter(body):
+	if body extends player_class && player == null:
 		set_process(true)
 		player = body
+		rotate_towards_player()
+		throw(ball)
+		start_throw_timer()
+
+func _on_throw_timer_timeout():
+	throw(ball)
